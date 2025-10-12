47ff520f3f9c90357168412d99d857d0567ac249
unknown <raphael.zwischelsberger@htl-braunau.at>
2025-10-12 16:51:11 +0200

    Renamed file for clarity

---
+++ src/server/api/routers/ticket.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "~/server/api/trpc";
+
+export const ticketRouter = createTRPCRouter({
+  all: protectedProcedure.query(async ({ ctx }) => {
+    const tickets = await ctx.db.soldTickets.findMany({
+      where: {
+        buyerId: parseInt(ctx.session.user.id),
+      },
+    });
+    return tickets.map(({ id, delivery, code, paid, sent, timestamp }) => ({ id, delivery, code, paid, sent, timestamp }));
+    return 'Test message from protected procedure'
+  })
+});
