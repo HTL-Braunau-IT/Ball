4d42cc912e43afb06960a07eaba36191d5f2fab2
unknown <raphael.zwischelsberger@htl-braunau.at>
2025-10-12 16:44:58 +0200

    Created endpoint for bought tickets

---
+++ src/server/api/routers/buyer.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "~/server/api/trpc";
+
+export const buyerRouter = createTRPCRouter({
+  all: protectedProcedure.query(async ({ ctx }) => {
+    const tickets = await ctx.db.soldTickets.findMany({
+      where: {
+        buyerId: parseInt(ctx.session.user.id),
+      },
+    });
+    return tickets.map(({ id, delivery, code, paid, sent, timestamp }) => ({ id, delivery, code, paid, sent, timestamp }));
+  })
+});
