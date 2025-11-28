830c5cf3cb6cfcd58da7225a0a1d69f4117ed245
Zwischelsberger Raphael <raphael.zwischelsberger@htl-braunau.at>
2025-11-28 07:43:49 +0100

    Test commit

--- src/app/backend/buyers/page.tsx
+++ src/app/backend/buyers/page.tsx
@@ -2,6 +2,7 @@ import Buyers from "~/components/buyers";
 
 export default function BuyersPage() {
   return (
+      // Test comment
       <div className="space-y-6">
         <Buyers />
       </div>
